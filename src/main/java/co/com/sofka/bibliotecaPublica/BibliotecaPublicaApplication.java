9dbaa34a72ea6ca135794f8fbceb87d12b82bcf6
Yenifer <guajeyenifer@gmail.com>
2022-01-13 21:59:13 -0500

    Se inicializa el proyecto de biblioteca publica con spring boot y mongoDB

---
+++ src/main/java/co/com/sofka/bibliotecaPublica/BibliotecaPublicaApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.bibliotecaPublica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaPublicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaPublicaApplication.class, args);
+	}
+
+}
